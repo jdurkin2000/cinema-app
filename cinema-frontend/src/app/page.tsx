66bf79201bd2dfe7029d300f7ef63ac2594208cb
jdurkin2000 <durkin.jfd@gmail.com>
2025-09-24 15:54:12 -0400

    Added movie model

--- cinema-frontend/src/app/page.tsx
+++ cinema-frontend/src/app/page.tsx
@@ -1,43 +1,56 @@
-"use client"
+"use client";
 
 import { useEffect, useState } from "react";
-import Image from 'next/image'
+import Image from "next/image";
+import Movie from "@/models/movie";
 
 export default function Home() {
-  const loadingMovie = {
+  const loadingMovie: Movie = {
+    _id: -1,
     title: "Loading movie data..",
     poster: "/poster_loading.png",
     synopsis: "Skibidi Toilet Dubai Chocolate Labubu",
-    trailer: "https://www.youtube.com/embed/dQw4w9WgXcQ"
-  }
+    trailer: "https://www.youtube.com/embed/dQw4w9WgXcQ",
+    genres: ["Weeb", "Horror"],
+    cast: ["Spongebob", "Patrick"],
+    director: "John Cena",
+    producer: "Danny Devito",
+    reviews: ["Yes", "Cool"],
+    rating: "NR",
+    showtimes: [""],
+    released:"2025-11-11",
+    isUpcoming: false,
+  };
   const [movie, setMovie] = useState(loadingMovie);
   useEffect(() => {
     fetch("http://localhost:8080/api/movies?title=slayer")
-      .then(res => res.json())
-      .then(data => setMovie(data[0]))
-      .catch(err => console.error(err))
-  })
+      .then((res) => res.json())
+      .then((data) => setMovie(data[0]))
+      .catch((err) => console.error(err));
+  });
 
   return (
     <div className="flex flex-col font-sans items-center justify-items-center">
-      <pre className="text-xl">{movie.trailer}</pre>
+      <pre className="text-xl">{movie.title}</pre>
+      <p>Rated: {movie.rating}</p>
       <p>{movie.synopsis}</p>
-      <Image
-        src={movie.poster}
-        alt="Movie Poster"
-        width={200}
-        height={250}
-      />
+      <Image src={movie.poster} alt="Movie Poster" width={200} height={250} />
       <div className="aspect-w-16 aspect-h-9">
-      <iframe
-        width="560"
-        height="315"
-        src={movie.trailer}
-        title="YouTube video player"
-        allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowFullScreen
-      />
-    </div>
+        <iframe
+          width="560"
+          height="315"
+          src={movie.trailer}
+          title="YouTube video player"
+          allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        />
+      </div>
+      <p>Available Showtimes:</p>
+      <ul>
+        {movie.showtimes.map((value, index) => (
+          <li key={index}>{value}</li>
+        ))}
+        </ul>
     </div>
   );
 }
