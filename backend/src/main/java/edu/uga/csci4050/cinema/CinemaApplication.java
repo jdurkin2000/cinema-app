69d0a3324ee8a2839fbc3ed2297fe30dd37878d4
jdurkin2000 <durkin.jfd@gmail.com>
2025-10-23 18:32:03 -0400

    application.properties is now pushed to git with encrypted info. To make app work, follow instructions in secrets.properties.template file

--- backend/src/main/java/edu/uga/csci4050/cinema/CinemaApplication.java
+++ backend/src/main/java/edu/uga/csci4050/cinema/CinemaApplication.java
@@ -1,12 +1,16 @@
 package edu.uga.csci4050.cinema;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
 @EnableMongoRepositories
+@EnableEncryptableProperties
+@PropertySource(name="EncryptedProperties", value = "classpath:application.properties")
 public class CinemaApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
