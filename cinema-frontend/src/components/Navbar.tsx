ade5b43eb37fb1348eaa8d4922732bf2beee4c2a
withers0 <j3remyazucena@gmail.com>
2025-11-23 14:11:53 -0500

    Made navbar look real nice

--- cinema-frontend/src/components/Navbar.tsx
+++ cinema-frontend/src/components/Navbar.tsx
@@ -3,7 +3,7 @@
 import "./Navbar.css"; // <--- just for NavBar
 import Image from "next/image";
 import Link from "next/link";
-import logo from "@/assets/logo.png";
+import logo from "@/assets/logo.svg";
 import { useAuth } from "@/app/AuthProvider";
 
 export default function Navbar() {
@@ -11,15 +11,16 @@ export default function Navbar() {
 
   return (
     <nav className="topnav">
-      <Image src={logo} alt="Site Logo" className="nav-logo" />
-      <h1 className="title">CINEMA</h1>
+      <Link href="/" className="nav-brand" style={{ display: "flex", alignItems: "center" }}>
+        <Image src={logo} alt="Site Logo" className="nav-logo" />
+        <h1 className="title">PeakCinema</h1>
+      </Link>
 
       <div className="nav-links">
         {user ? (
           <>
-            <Link href="/profile" className="text-white font-semibold">
-              Welcome, {user}
-            </Link>
+            <span className="nav-welcome">Welcome, {user}</span>
+            <Link href="/">Home</Link>
             <Link href="/profile">Edit Profile</Link>
             <button onClick={logout}>Logout</button>
           </>
